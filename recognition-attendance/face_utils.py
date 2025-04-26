4c855bc32aafbc5a1cdaac7def1de6d9faed2438
Rohit Karthick <rkbeo5@gmail.com>
2025-04-27 01:39:08 +0530

    refactor: merged manage_users into face_utils

--- recognition-attendance/face_utils.py
+++ recognition-attendance/face_utils.py
@@ -81,6 +81,52 @@ def unknown_list(img):
     print(f"Saved unknown face as {filename}")
     return name
 
+# Mange Users
+
+def add_new_user(img):
+    save_path = 'recognition-attendance/base-images'
+
+    rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    encodings = face_recognition.face_encodings(rgb_img)
+
+    if not encodings:
+        print("No face found in the frame.")
+        return None, None, None
+
+    name = input("Enter name for the new face: ").strip()
+    filename = f"{name}.jpg"
+    full_path = os.path.join(save_path, filename)
+
+    # Save image
+    cv2.imwrite(full_path, img)
+    print(f"Saved new face image as: {full_path}")
+
+    return encodings[0], name, img
+
+def remove_user(user_name, encodings_list, class_names, image_list):
+    if user_name in class_names:
+        idx = class_names.index(user_name)
+        
+        file_dir = 'recognition-attendance/base-images'
+        file_path = os.path.join(file_dir, f"{user_name}.jpg")
+        
+        # Remove from memory
+        class_names.pop(idx)
+        encodings_list.pop(idx)
+        image_list.pop(idx)
+        
+        # Delete image file from disk
+        if os.path.exists(file_path):
+            os.remove(file_path)
+            print(f"Image '{file_path}' deleted.")
+        else:
+            print(f"Image file for user '{user_name}' not found.")
+
+        print(f"User '{user_name}' removed successfully.")
+
+    else:
+        print(f"User '{user_name}' not found.")
+
 def clear_unknown_faces_local(local_dir="recognition-attendance/unknowns"):
  
     if os.path.exists(local_dir):
